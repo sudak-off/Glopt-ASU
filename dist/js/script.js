a0e2c6b6a5132dbb367907cb2c9717d0c5af09d9
Artyom <krestonos@gmail.com>
2021-12-06 15:16:23 +0200

    add section consultation

--- dist/js/script.js
+++ dist/js/script.js
@@ -2,13 +2,13 @@ $(document).ready(function(){
 	
 	function toggleSlide(item) {
 	    $(item).each(function(i) {
-		  $(this).on('click', function(e) {
-			e.preventDefault();
-			$('.price__info').eq(i).toggleClass('price__info_active');
-			$('.price__list').eq(i).toggleClass('price__list_active');
-		  })
+		    $(this).on('click', function (e) {
+			    e.preventDefault();
+			    $('.price__info').eq(i).toggleClass('price__info_active');
+			    $('.price__list').eq(i).toggleClass('price__list_active');
+		    });
 	    });
-	};
+	}
   
 	toggleSlide('.price__btn');
 	toggleSlide('.price__back');
