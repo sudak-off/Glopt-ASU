1c1e2434a2d1dfed46c4be8c86a66c42fab1768b
Artyom <krestonos@gmail.com>
2021-12-11 16:54:58 +0200

    add section carousel

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,7 @@
 $(document).ready(function(){
 	
+	//tabs 
+
 	function toggleSlide(item) {
 	    $(item).each(function(i) {
 		    $(this).on('click', function (e) {
@@ -13,6 +15,14 @@ $(document).ready(function(){
 	toggleSlide('.price__btn');
 	toggleSlide('.price__back');
 	
-	
+	// slide 
+
+	$('.carousel__inner').slick({
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		prevArrow: '<button type="button" class="slick-prev"><img src="img/slick_prev.png" alt="prev"></button>',
+		nextArrow: '<button type="button" class="slick-next"><img src="img/slock_next.png" alt="next"></button>',
+	});
+	    
   });
   
