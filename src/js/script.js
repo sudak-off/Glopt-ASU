35c46f51c1861415d96f3f3df048ab5f58579a30
Artyom <krestonos@gmail.com>
2021-12-13 06:06:23 +0200

    add section sorousel + add slider

--- src/js/script.js
+++ src/js/script.js
@@ -16,7 +16,12 @@ $(document).ready(function(){
 	toggleSlide('.price__back');
 	
 	// slide 
-	
+	$('.carousel__inner').slick({
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		prevArrow: '<button type="button" class="slick-prev"><img src="img/slick_prev.png" alt="prev"></button>',
+		nextArrow: '<button type="button" class="slick-next"><img src="img/slock_next.png" alt="next"></button>',
+	});
 	    
 	// modal 
 	
